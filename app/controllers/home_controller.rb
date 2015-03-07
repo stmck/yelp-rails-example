3e7a6ff9a4a12e57ce1d91c45f9195e8a35e56b5
michiko <sato@michiko-no-MacBook-Pro.local>
2015-03-07 15:26:59 +0900

    神奈川区で検索ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,23 +2,10 @@ class HomeController < ApplicationController
   def index
   end
 
-  def search
-
+def search
+	#binding.pry
     parameters = { term:'野菜-ベジタリアン', limit: 16 }
-    render json: Yelp.client.search('Tokyo', parameters)    
-
-    #①言葉を入れて、サーチボタン押すだけで、何でもそれに関するジャンルのお店がでる。
-    #parameters = { term: params[:term], limit: 16 }
-    #render json: Yelp.client.search('Tokyo', parameters)
-
-    #②サーチボタン押すだけで、野菜に関する店がでる。
-    #parameters = { term:'野菜', limit: 20 }
-    #render json: Yelp.client.search('Tokyo', parameters)
-
-    #termは、複数。ビーガン、インド料理、ベジタリアン、マクロビの複数カテゴリ設定にはできない？
-    #場所は、日本全国から検索したい。 ユーザーが【場所】と、【食べ物の名前】などを入力して、その地域のお店がでる。
-    #ちずの連動 ヴィーガン
-
-  end
+    render json: Yelp.client.search(params[:term], parameters) 
 end
-	
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'map/index'
-
-  resources :users
-
   root 'home#index'
   post '/search' => 'home#search'
 
